071b02dcf9e01d01db32d4d952a0307d6d94c2eb
Paul Phyo <paulphyo25@gmail.com>
2025-07-31 14:30:01 +0800

    fix(terraform): explicit ssh & ports in terraform

--- infra/terraform/env/dev/main.tf
+++ infra/terraform/env/dev/main.tf
@@ -11,13 +11,53 @@ terraform {
   }
 }
 
+
+resource "aws_security_group" "simbus_security_terraform" {
+  name        = "app-security-group"
+  description = "Allow SSH, frontend and backend traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow frontend HTTP"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow backend HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "simbus" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.simbus_security_terraform.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "simbus-instance"
   }
 }
-
