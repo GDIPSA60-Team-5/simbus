5e3921b7a6966110d58926405d9b247af0863258
coderbang-bang <E1509801@u.nus.edu>
2025-08-02 15:48:31 +0800

    add userName and password for testing

--- spring-backend/src/main/java/com/example/springbackend/SpringBackendApplication.java
+++ spring-backend/src/main/java/com/example/springbackend/SpringBackendApplication.java
@@ -1,7 +1,12 @@
 package com.example.springbackend;
 
+import com.example.springbackend.repository.UserRepository;
+import com.example.springbackend.model.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
@@ -10,6 +15,23 @@ public class SpringBackendApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringBackendApplication.class, args);
+
     }
+
+
+    //input userName(user) and passwordHash(password) into database when run application
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByUserName("user").isEmpty()) {
+                User user = new User();
+                user.setUserName("user");
+                user.setUserType("admin");
+                user.setPasswordHash(passwordEncoder.encode("password"));
+                userRepository.save(user);
+            }
+        };
+    }
+
 
 }
