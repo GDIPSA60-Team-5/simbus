1d68e8681d9abcbfe63465f38c17b7f27ea49622
Paul Phyo <paulphyo25@gmail.com>
2025-08-16 20:28:19 +0800

    fix: trivy vulnerabilities

--- spring-backend/src/main/java/com/example/springbackend/security/JwtTokenProvider.java
+++ spring-backend/src/main/java/com/example/springbackend/security/JwtTokenProvider.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -18,8 +17,8 @@ public class JwtTokenProvider {
     @Value("${jwt.expiration-in-ms}")
     private long jwtExpirationInMs;
 
-    public JwtTokenProvider(@Value("${jwt.secret}") String secretBase64) {
-        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretBase64));
+    public JwtTokenProvider() {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode("8acf55412a0a9f4c7cd9e8a099f309df3580c0bdc42bf2da4ccf3122e087ed8f"));
     }
 
     public String generateToken(String username) {
