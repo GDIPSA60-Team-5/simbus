9bbe546ec3c281c74621637628dcaf65ce234bde
Paul Phyo <paulphyo25@gmail.com>
2025-08-16 20:40:03 +0800

    fix: jwt token

--- spring-backend/src/main/java/com/example/springbackend/security/JwtTokenProvider.java
+++ spring-backend/src/main/java/com/example/springbackend/security/JwtTokenProvider.java
@@ -17,8 +17,8 @@ public class JwtTokenProvider {
     @Value("${jwt.expiration-in-ms}")
     private long jwtExpirationInMs;
 
-    public JwtTokenProvider() {
-        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode("8acf55412a0a9f4c7cd9e8a099f309df3580c0bdc42bf2da4ccf3122e087ed8f"));
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretBase64) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretBase64));
     }
 
     public String generateToken(String username) {
